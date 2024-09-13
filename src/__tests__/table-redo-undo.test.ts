2098389da32b13751826680c93369c3f068e450f
zzxming <zzxming77@163.com>
2024-09-13 18:29:49 +0800

    test: add undo test

--- src/__tests__/table-redo-undo.test.ts
+++ src/__tests__/table-redo-undo.test.ts
@@ -1,7 +1,7 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 import type TableUp from '../index';
 import { TableCellInnerFormat, TableSelection } from '../index';
-import { createTable, createTableHTML } from './utils';
+import { createTable, createTableHTML, createTaleColHTML } from './utils';
 
 beforeEach(() => {
   vi.useFakeTimers();
@@ -230,6 +230,269 @@ describe('table undo', () => {
             </tbody>
           </table>
         </div>
+        <p><br></p>
+      `,
+      { ignoreAttrs: ['class', 'style', 'data-table-id', 'contenteditable'] },
+    );
+  });
+
+  it('merge single row undo', async () => {
+    const quill = await createTable(3, 3);
+    const tableModule = quill.getModule('tableUp') as TableUp;
+    const table = quill.root.querySelector('table')!;
+    tableModule.tableSelection = new TableSelection(tableModule, table, quill);
+    const tds = quill.scroll.descendants(TableCellInnerFormat, 0);
+    tableModule.tableSelection.selectedTds = [tds[0], tds[1], tds[2]];
+    tableModule.mergeCells();
+    await vi.runAllTimersAsync();
+    quill.history.undo();
+    await vi.runAllTimersAsync();
+    expect(quill.root).toEqualHTML(
+      `
+        <p><br></p>
+        ${createTableHTML(3, 3)}
+        <p><br></p>
+      `,
+      { ignoreAttrs: ['class', 'style', 'data-table-id', 'contenteditable'] },
+    );
+  });
+
+  it('merge multiple row undo', async () => {
+    const quill = await createTable(4, 4);
+    const tableModule = quill.getModule('tableUp') as TableUp;
+    const table = quill.root.querySelector('table')!;
+    tableModule.tableSelection = new TableSelection(tableModule, table, quill);
+    const tds = quill.scroll.descendants(TableCellInnerFormat, 0);
+    tableModule.tableSelection.selectedTds = [tds[0], tds[1], tds[2], tds[3], tds[4], tds[5], tds[6], tds[7], tds[8], tds[9], tds[10], tds[11]];
+    tableModule.mergeCells();
+    await vi.runAllTimersAsync();
+    quill.history.undo();
+    await vi.runAllTimersAsync();
+    expect(quill.root).toEqualHTML(
+      `
+        <p><br></p>
+        ${createTableHTML(4, 4)}
+        <p><br></p>
+      `,
+      { ignoreAttrs: ['class', 'style', 'data-table-id', 'contenteditable'] },
+    );
+  });
+
+  it('merge middle row undo', async () => {
+    const quill = await createTable(4, 4);
+    const tableModule = quill.getModule('tableUp') as TableUp;
+    const table = quill.root.querySelector('table')!;
+    tableModule.tableSelection = new TableSelection(tableModule, table, quill);
+    const tds = quill.scroll.descendants(TableCellInnerFormat, 0);
+    tableModule.tableSelection.selectedTds = [tds[4], tds[5], tds[6], tds[7], tds[8], tds[9], tds[10], tds[11]];
+    tableModule.mergeCells();
+    await vi.runAllTimersAsync();
+    quill.history.undo();
+    await vi.runAllTimersAsync();
+    expect(quill.root).toEqualHTML(
+      `
+        <p><br></p>
+        ${createTableHTML(4, 4)}
+        <p><br></p>
+      `,
+      { ignoreAttrs: ['class', 'style', 'data-table-id', 'contenteditable'] },
+    );
+  });
+
+  it('5x5 merge center 3x3 cells undo', async () => {
+    const quill = await createTable(5, 5);
+    const tableModule = quill.getModule('tableUp') as TableUp;
+    const table = quill.root.querySelector('table')!;
+    tableModule.tableSelection = new TableSelection(tableModule, table, quill);
+    const tds = quill.scroll.descendants(TableCellInnerFormat, 0);
+    tableModule.tableSelection.selectedTds = [tds[6], tds[7], tds[8], tds[11], tds[12], tds[13], tds[16], tds[17], tds[18]];
+    tableModule.mergeCells();
+    await vi.runAllTimersAsync();
+    quill.history.undo();
+    await vi.runAllTimersAsync();
+    expect(quill.root).toEqualHTML(
+      `
+        <p><br></p>
+        ${createTableHTML(5, 5)}
+        <p><br></p>
+      `,
+      { ignoreAttrs: ['class', 'style', 'data-table-id', 'contenteditable'] },
+    );
+  });
+
+  it('4x4 undo split 3x3 at start 1', async () => {
+    const quill = await createTable(4, 4);
+    const tableModule = quill.getModule('tableUp') as TableUp;
+    const table = quill.root.querySelector('table')!;
+    tableModule.tableSelection = new TableSelection(tableModule, table, quill);
+    const tds = quill.scroll.descendants(TableCellInnerFormat, 0);
+    tableModule.tableSelection.selectedTds = [tds[0], tds[1], tds[2], tds[4], tds[5], tds[6], tds[8], tds[9], tds[10]];
+    tableModule.mergeCells();
+    await vi.runAllTimersAsync();
+    tableModule.tableSelection.selectedTds = [tds[0]];
+    tableModule.splitCell();
+    await vi.runAllTimersAsync();
+    quill.history.undo();
+    await vi.runAllTimersAsync();
+    expect(quill.root).toEqualHTML(
+      `
+        <p><br></p>
+        <div>
+          <table cellpadding="0" cellspacing="0" data-full="true">
+            <colgroup data-full="true">
+              <col width="25%" data-col-id="1" data-full="true" />
+              <col width="25%" data-col-id="2" data-full="true" />
+              <col width="25%" data-col-id="3" data-full="true" />
+              <col width="25%" data-col-id="4" data-full="true" />
+            </colgroup>
+            <tbody>
+              <tr data-row-id="1">
+                <td rowspan="3" colspan="3" data-col-id="1" data-row-id="1">
+                  <div data-rowspan="3" data-colspan="3" data-row-id="1" data-col-id="1">
+                    <p>1</p>
+                    <p>2</p>
+                    <p>3</p>
+                    <p>5</p>
+                    <p>6</p>
+                    <p>7</p>
+                    <p>9</p>
+                    <p>10</p>
+                    <p>11</p>
+                  </div>
+                </td>
+                <td rowspan="1" colspan="1" data-col-id="4" data-row-id="1">
+                  <div data-rowspan="1" data-colspan="1" data-row-id="1" data-col-id="4"><p>4</p></div>
+                </td>
+              </tr>
+              <tr data-row-id="2">
+                <td rowspan="1" colspan="1" data-col-id="4" data-row-id="2">
+                  <div data-rowspan="1" data-colspan="1" data-row-id="2" data-col-id="4"><p>8</p></div>
+                </td>
+              </tr>
+              <tr data-row-id="3">
+                <td rowspan="1" colspan="1" data-col-id="4" data-row-id="3">
+                  <div data-rowspan="1" data-colspan="1" data-row-id="3" data-col-id="4"><p>12</p></div>
+                </td>
+              </tr>
+              <tr data-row-id="4">
+                ${
+                  [13, 14, 15, 16].map((num, i) => `
+                    <td rowspan="1" colspan="1" data-col-id="${i + 1}" data-row-id="4">
+                      <div data-rowspan="1" data-colspan="1" data-row-id="4" data-col-id="${i + 1}"><p>${num}</p></div>
+                    </td>
+                  `).join('\n')
+                }
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <p><br></p>
+      `,
+      { ignoreAttrs: ['class', 'style', 'data-table-id', 'contenteditable'] },
+    );
+    quill.history.undo();
+    await vi.runAllTimersAsync();
+    expect(quill.root).toEqualHTML(
+      `
+        <p><br></p>
+        ${createTableHTML(4, 4)}
+        <p><br></p>
+      `,
+      { ignoreAttrs: ['class', 'style', 'data-table-id', 'contenteditable'] },
+    );
+  });
+
+  it('5x5 undo split 3x3 at end 25 and undo merge 3x3 at end 25', async () => {
+    const quill = await createTable(5, 5);
+    const tableModule = quill.getModule('tableUp') as TableUp;
+    const table = quill.root.querySelector('table')!;
+    tableModule.tableSelection = new TableSelection(tableModule, table, quill);
+    const tds = quill.scroll.descendants(TableCellInnerFormat, 0);
+    tableModule.tableSelection.selectedTds = [tds[12], tds[13], tds[14], tds[17], tds[18], tds[19], tds[22], tds[23], tds[24]];
+    tableModule.mergeCells();
+    await vi.runAllTimersAsync();
+    tableModule.tableSelection.selectedTds = [tds[12]];
+    tableModule.splitCell();
+    await vi.runAllTimersAsync();
+    quill.history.undo();
+    await vi.runAllTimersAsync();
+    expect(quill.root).toEqualHTML(
+      `
+        <p><br></p>
+        <div>
+          <table cellpadding="0" cellspacing="0" data-full="true">
+            ${createTaleColHTML(5)}
+            <tbody>
+              <tr data-row-id="1">
+                ${
+                  [1, 2, 3, 4, 5].map((n, i) => `
+                    <td rowspan="1" colspan="1" data-col-id="${i + 1}" data-row-id="1">
+                      <div data-rowspan="1" data-colspan="1" data-row-id="1" data-col-id="${i + 1}"><p>${n}</p></div>
+                    </td>
+                  `).join('\n')
+                }
+              </tr>
+              <tr data-row-id="2">
+                ${
+                  [6, 7, 8, 9, 10].map((n, i) => `
+                    <td rowspan="1" colspan="1" data-col-id="${i + 1}" data-row-id="2">
+                      <div data-rowspan="1" data-colspan="1" data-row-id="2" data-col-id="${i + 1}"><p>${n}</p></div>
+                    </td>
+                  `).join('\n')
+                }
+              </tr>
+              <tr data-row-id="3">
+                <td rowspan="1" colspan="1" data-col-id="1" data-row-id="3">
+                  <div data-rowspan="1" data-colspan="1" data-row-id="3" data-col-id="1"><p>11</p></div>
+                </td>
+                <td rowspan="1" colspan="1" data-col-id="2" data-row-id="3">
+                  <div data-rowspan="1" data-colspan="1" data-row-id="3" data-col-id="2"><p>12</p></div>
+                </td>
+                <td rowspan="3" colspan="3" data-col-id="3" data-row-id="3">
+                  <div data-rowspan="3" data-colspan="3" data-row-id="3" data-col-id="3">
+                    <p>13</p>
+                    <p>14</p>
+                    <p>15</p>
+                    <p>18</p>
+                    <p>19</p>
+                    <p>20</p>
+                    <p>23</p>
+                    <p>24</p>
+                    <p>25</p>
+                  </div>
+                </td>
+              </tr>
+              <tr data-row-id="4">
+                ${
+                  [16, 17].map((n, i) => `
+                    <td rowspan="1" colspan="1" data-col-id="${i + 1}" data-row-id="4">
+                      <div data-rowspan="1" data-colspan="1" data-row-id="4" data-col-id="${i + 1}"><p>${n}</p></div>
+                    </td>
+                  `).join('\n')
+                }
+              </tr>
+              <tr data-row-id="5">
+                ${
+                  [21, 22].map((n, i) => `
+                    <td rowspan="1" colspan="1" data-col-id="${i + 1}" data-row-id="5">
+                      <div data-rowspan="1" data-colspan="1" data-row-id="5" data-col-id="${i + 1}"><p>${n}</p></div>
+                    </td>
+                  `).join('\n')
+                }
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <p><br></p>
+      `,
+      { ignoreAttrs: ['class', 'style', 'data-table-id', 'contenteditable'] },
+    );
+    quill.history.undo();
+    await vi.runAllTimersAsync();
+    expect(quill.root).toEqualHTML(
+      `
+        <p><br></p>
+        ${createTableHTML(5, 5)}
         <p><br></p>
       `,
       { ignoreAttrs: ['class', 'style', 'data-table-id', 'contenteditable'] },
