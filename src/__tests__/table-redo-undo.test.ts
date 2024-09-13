d6cbd8fdfcf1e39282ec282c5cb31bd2838d9227
zzxming <zzxming77@163.com>
2024-09-13 10:08:53 +0800

    style: fix indent

--- src/__tests__/table-redo-undo.test.ts
+++ src/__tests__/table-redo-undo.test.ts
@@ -59,7 +59,7 @@ const createTableHTML = (row: number, col: number) => {
   `;
 };
 
-describe('table redo', () => {
+describe('table undo', () => {
   it('merge all cell undo', async () => {
     const quill = await createTable(3, 3);
     const tableModule = quill.getModule('tableUp') as TableUp;
@@ -73,10 +73,10 @@ describe('table redo', () => {
     await vi.runAllTimersAsync();
     expect(quill.root).toEqualHTML(
       `
-      <p><br></p>
-      ${createTableHTML(3, 3)}
-      <p><br></p>
-    `,
+        <p><br></p>
+        ${createTableHTML(3, 3)}
+        <p><br></p>
+      `,
       { ignoreAttrs: ['class', 'style', 'data-table-id', 'contenteditable'] },
     );
   });
@@ -94,10 +94,10 @@ describe('table redo', () => {
     await vi.runAllTimersAsync();
     expect(quill.root).toEqualHTML(
       `
-      <p><br></p>
-      ${createTableHTML(3, 3)}
-      <p><br></p>
-    `,
+        <p><br></p>
+        ${createTableHTML(3, 3)}
+        <p><br></p>
+      `,
       { ignoreAttrs: ['class', 'style', 'data-table-id', 'contenteditable'] },
     );
   });
@@ -160,49 +160,49 @@ describe('table redo', () => {
     await vi.runAllTimersAsync();
     expect(quill.root).toEqualHTML(
       `
-      <p><br></p>
-      <div>
-        <table cellpadding="0" cellspacing="0" data-full="true">
-          <colgroup data-full="true">
-            ${new Array(3).fill(0).map((_, i) => `<col width="${1 / 3 * 100}%" data-col-id="${i + 1}" data-full="true" />`).join('\n')}
-          </colgroup>
-          <tbody>
-            <tr data-row-id="1">
-              <td rowspan="1" colspan="1" data-col-id="1" data-row-id="1">
-                <div data-rowspan="1" data-colspan="1" data-row-id="1" data-col-id="1"><p>1</p></div>
-              </td>
-              <td rowspan="1" colspan="1" data-col-id="2" data-row-id="1">
-                <div data-rowspan="1" data-colspan="1" data-row-id="1" data-col-id="2"><p>2</p></div>
-              </td>
-              <td rowspan="3" colspan="1" data-col-id="3" data-row-id="1">
-                <div data-rowspan="3" data-colspan="1" data-row-id="1" data-col-id="3">
-                  <p>3</p>
-                  <p>6</p>
-                  <p>9</p>
-                </div>
-              </td>
-            </tr>
-            <tr data-row-id="2">
-              <td rowspan="1" colspan="1" data-col-id="1" data-row-id="2">
-                <div data-rowspan="1" data-colspan="1" data-row-id="2" data-col-id="1"><p>4</p></div>
-              </td>
-              <td rowspan="1" colspan="1" data-col-id="2" data-row-id="2">
-                <div data-rowspan="1" data-colspan="1" data-row-id="2" data-col-id="2"><p>5</p></div>
-              </td>
-            </tr>
-            <tr data-row-id="3">
-              <td rowspan="1" colspan="1" data-col-id="1" data-row-id="3">
-                <div data-rowspan="1" data-colspan="1" data-row-id="3" data-col-id="1"><p>7</p></div>
-              </td>
-              <td rowspan="1" colspan="1" data-col-id="2" data-row-id="3">
-                <div data-rowspan="1" data-colspan="1" data-row-id="3" data-col-id="2"><p>8</p></div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <p><br></p>
-    `,
+        <p><br></p>
+        <div>
+          <table cellpadding="0" cellspacing="0" data-full="true">
+            <colgroup data-full="true">
+              ${new Array(3).fill(0).map((_, i) => `<col width="${1 / 3 * 100}%" data-col-id="${i + 1}" data-full="true" />`).join('\n')}
+            </colgroup>
+            <tbody>
+              <tr data-row-id="1">
+                <td rowspan="1" colspan="1" data-col-id="1" data-row-id="1">
+                  <div data-rowspan="1" data-colspan="1" data-row-id="1" data-col-id="1"><p>1</p></div>
+                </td>
+                <td rowspan="1" colspan="1" data-col-id="2" data-row-id="1">
+                  <div data-rowspan="1" data-colspan="1" data-row-id="1" data-col-id="2"><p>2</p></div>
+                </td>
+                <td rowspan="3" colspan="1" data-col-id="3" data-row-id="1">
+                  <div data-rowspan="3" data-colspan="1" data-row-id="1" data-col-id="3">
+                    <p>3</p>
+                    <p>6</p>
+                    <p>9</p>
+                  </div>
+                </td>
+              </tr>
+              <tr data-row-id="2">
+                <td rowspan="1" colspan="1" data-col-id="1" data-row-id="2">
+                  <div data-rowspan="1" data-colspan="1" data-row-id="2" data-col-id="1"><p>4</p></div>
+                </td>
+                <td rowspan="1" colspan="1" data-col-id="2" data-row-id="2">
+                  <div data-rowspan="1" data-colspan="1" data-row-id="2" data-col-id="2"><p>5</p></div>
+                </td>
+              </tr>
+              <tr data-row-id="3">
+                <td rowspan="1" colspan="1" data-col-id="1" data-row-id="3">
+                  <div data-rowspan="1" data-colspan="1" data-row-id="3" data-col-id="1"><p>7</p></div>
+                </td>
+                <td rowspan="1" colspan="1" data-col-id="2" data-row-id="3">
+                  <div data-rowspan="1" data-colspan="1" data-row-id="3" data-col-id="2"><p>8</p></div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <p><br></p>
+      `,
       { ignoreAttrs: ['class', 'style', 'data-table-id', 'contenteditable'] },
     );
   });
@@ -223,64 +223,64 @@ describe('table redo', () => {
     await vi.runAllTimersAsync();
     expect(quill.root).toEqualHTML(
       `
-      <p><br></p>
-      <div>
-        <table cellpadding="0" cellspacing="0" data-full="true">
-          <colgroup data-full="true">
-            <col width="25%" data-col-id="1" data-full="true" />
-            <col width="50%" data-col-id="2" data-full="true" />
-            <col width="25%" data-col-id="4" data-full="true" />
-          </colgroup>
-          <tbody>
-            <tr data-row-id="1">
-              <td rowspan="1" colspan="1" data-col-id="1" data-row-id="1">
-                <div data-rowspan="1" data-colspan="1" data-row-id="1" data-col-id="1"><p>1</p></div>
-              </td>
-              <td rowspan="4" colspan="1" data-col-id="2" data-row-id="1">
-                <div data-rowspan="4" data-colspan="1" data-row-id="1" data-col-id="2">
-                  <p>2</p>
-                  <p>3</p>
-                  <p>6</p>
-                  <p>7</p>
-                  <p>10</p>
-                  <p>11</p>
-                  <p>14</p>
-                  <p>15</p>
-                </div>
-              </td>
-              <td rowspan="1" colspan="1" data-col-id="4" data-row-id="1">
-                <div data-rowspan="1" data-colspan="1" data-row-id="1" data-col-id="4"><p>4</p></div>
-              </td>
-            </tr>
-            <tr data-row-id="2">
-              <td rowspan="1" colspan="1" data-col-id="1" data-row-id="2">
-                <div data-rowspan="1" data-colspan="1" data-row-id="2" data-col-id="1"><p>5</p></div>
-              </td>
-              <td rowspan="1" colspan="1" data-col-id="4" data-row-id="2">
-                <div data-rowspan="1" data-colspan="1" data-row-id="2" data-col-id="4"><p>8</p></div>
-              </td>
-            </tr>
-            <tr data-row-id="3">
-              <td rowspan="1" colspan="1" data-col-id="1" data-row-id="3">
-                <div data-rowspan="1" data-colspan="1" data-row-id="3" data-col-id="1"><p>9</p></div>
-              </td>
-              <td rowspan="1" colspan="1" data-col-id="4" data-row-id="3">
-                <div data-rowspan="1" data-colspan="1" data-row-id="3" data-col-id="4"><p>12</p></div>
-              </td>
-            </tr>
-            <tr data-row-id="4">
-              <td rowspan="1" colspan="1" data-col-id="1" data-row-id="4">
-                <div data-rowspan="1" data-colspan="1" data-row-id="4" data-col-id="1"><p>13</p></div>
-              </td>
-              <td rowspan="1" colspan="1" data-col-id="4" data-row-id="4">
-                <div data-rowspan="1" data-colspan="1" data-row-id="4" data-col-id="4"><p>16</p></div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <p><br></p>
-    `,
+        <p><br></p>
+        <div>
+          <table cellpadding="0" cellspacing="0" data-full="true">
+            <colgroup data-full="true">
+              <col width="25%" data-col-id="1" data-full="true" />
+              <col width="50%" data-col-id="2" data-full="true" />
+              <col width="25%" data-col-id="4" data-full="true" />
+            </colgroup>
+            <tbody>
+              <tr data-row-id="1">
+                <td rowspan="1" colspan="1" data-col-id="1" data-row-id="1">
+                  <div data-rowspan="1" data-colspan="1" data-row-id="1" data-col-id="1"><p>1</p></div>
+                </td>
+                <td rowspan="4" colspan="1" data-col-id="2" data-row-id="1">
+                  <div data-rowspan="4" data-colspan="1" data-row-id="1" data-col-id="2">
+                    <p>2</p>
+                    <p>3</p>
+                    <p>6</p>
+                    <p>7</p>
+                    <p>10</p>
+                    <p>11</p>
+                    <p>14</p>
+                    <p>15</p>
+                  </div>
+                </td>
+                <td rowspan="1" colspan="1" data-col-id="4" data-row-id="1">
+                  <div data-rowspan="1" data-colspan="1" data-row-id="1" data-col-id="4"><p>4</p></div>
+                </td>
+              </tr>
+              <tr data-row-id="2">
+                <td rowspan="1" colspan="1" data-col-id="1" data-row-id="2">
+                  <div data-rowspan="1" data-colspan="1" data-row-id="2" data-col-id="1"><p>5</p></div>
+                </td>
+                <td rowspan="1" colspan="1" data-col-id="4" data-row-id="2">
+                  <div data-rowspan="1" data-colspan="1" data-row-id="2" data-col-id="4"><p>8</p></div>
+                </td>
+              </tr>
+              <tr data-row-id="3">
+                <td rowspan="1" colspan="1" data-col-id="1" data-row-id="3">
+                  <div data-rowspan="1" data-colspan="1" data-row-id="3" data-col-id="1"><p>9</p></div>
+                </td>
+                <td rowspan="1" colspan="1" data-col-id="4" data-row-id="3">
+                  <div data-rowspan="1" data-colspan="1" data-row-id="3" data-col-id="4"><p>12</p></div>
+                </td>
+              </tr>
+              <tr data-row-id="4">
+                <td rowspan="1" colspan="1" data-col-id="1" data-row-id="4">
+                  <div data-rowspan="1" data-colspan="1" data-row-id="4" data-col-id="1"><p>13</p></div>
+                </td>
+                <td rowspan="1" colspan="1" data-col-id="4" data-row-id="4">
+                  <div data-rowspan="1" data-colspan="1" data-row-id="4" data-col-id="4"><p>16</p></div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <p><br></p>
+      `,
       { ignoreAttrs: ['class', 'style', 'data-table-id', 'contenteditable'] },
     );
   });
