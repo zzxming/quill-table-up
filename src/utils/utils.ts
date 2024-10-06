2bf1b1a898bde4136e9b2ae045fa8e2a1b7597b3
zzxming <zzxming77@163.com>
2024-10-06 10:34:30 +0800

    feat: add limit padding

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -94,26 +94,26 @@ export function mixinClass<
 
   return targetClass;
 };
-
+const viewportPadding = 8;
 export const limitDomInViewPort = (rect: { left: number; top: number; width: number; height: number }) => {
   let { left, top, width, height } = rect;
   const { clientWidth, clientHeight } = document.documentElement;
   let leftLimited = false;
   let topLimited = false;
   if (left + width > clientWidth) {
-    left = clientWidth - width;
+    left = clientWidth - width - viewportPadding;
     leftLimited = true;
   }
   else if (left < 0) {
-    left = 0;
+    left = viewportPadding;
     leftLimited = true;
   }
   if (top + height > clientHeight) {
-    top = clientHeight - height;
+    top = clientHeight - height - viewportPadding;
     topLimited = true;
   }
   else if (top < 0) {
-    top = 0;
+    top = viewportPadding;
     topLimited = true;
   }
   return {
