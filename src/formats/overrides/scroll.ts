15e8edbdf5c01f10ed0de703fd5c3cf469c4f04d
zzxming <zzxming77@163.com>
2024-12-18 22:42:40 +0800

    fix: html paste convert

--- src/formats/overrides/scroll.ts
+++ src/formats/overrides/scroll.ts
@@ -1,6 +1,7 @@
 import type { Parchment as TypeParchment } from 'quill';
 import Quill from 'quill';
 import { blotName } from '../../utils';
+import { TableCellInnerFormat } from '../table-cell-inner-format';
 
 const Parchment = Quill.import('parchment');
 const ScrollBlot = Quill.import('blots/scroll') as any;
@@ -9,14 +10,19 @@ export class ScrollOverride extends ScrollBlot {
   createBlock(attributes: Record<string, any>, refBlot?: TypeParchment.Blot) {
     let createBlotName: string | undefined;
     let formats: Record<string, any> = {};
-    // if attributes have not only one block blot. will save last. that will conflict with list/header in tableCellInner
-    for (const [key, value] of Object.entries(attributes)) {
-      const isBlockBlot = this.query(key, Parchment.Scope.BLOCK & Parchment.Scope.BLOT) != null;
-      if (isBlockBlot) {
-        createBlotName = key;
-      }
-      else {
-        formats[key] = value;
+    if (attributes[blotName.tableCellInner]) {
+      createBlotName = blotName.tableCellInner;
+    }
+    else {
+      // if attributes have not only one block blot. will save last. that will conflict with list/header in tableCellInner
+      for (const [key, value] of Object.entries(attributes)) {
+        const isBlockBlot = this.query(key, Parchment.Scope.BLOCK & Parchment.Scope.BLOT) != null;
+        if (isBlockBlot) {
+          createBlotName = key;
+        }
+        else {
+          formats[key] = value;
+        }
       }
     }
     // only add this judgement to merge block blot at table cell
@@ -32,7 +38,10 @@ export class ScrollOverride extends ScrollBlot {
 
     this.insertBefore(block, refBlot || undefined);
 
-    const length = block.length();
+    let length = block.length();
+    if (block instanceof TableCellInnerFormat && length === 0) {
+      length += 1;
+    }
     for (const [key, value] of Object.entries(formats)) {
       block.formatAt(0, length, key, value);
     }
