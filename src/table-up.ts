51debafba7ce062776e10af7ab43b8fc3ddc09ce
zzxming <zzxming77@163.com>
2025-06-01 21:48:28 +0800

    refactor: move sort and fix table together

--- src/table-up.ts
+++ src/table-up.ts
@@ -960,32 +960,23 @@ export class TableUp {
       Quill.events.SCROLL_OPTIMIZE,
       (mutations: MutationRecord[]) => {
         mutations.some((mutation) => {
-          const mutationTarget = mutation.target as HTMLElement;
-          if (mutationTarget.tagName === 'TABLE') {
-            const tableMain = Quill.find(mutationTarget) as TableMainFormat;
-            if (tableMain) {
-              tableMain.sortMergeChildren();
-              return true;
-            }
-          }
-          return false;
-        });
-      },
-    );
-
-    this.quill.on(
-      Quill.events.SCROLL_OPTIMIZE,
-      (mutations: MutationRecord[]) => {
-        mutations.some((mutation) => {
-          if (
-            // TODO: if need add ['COL', 'COLGROUP']
-            ['TD', 'TR', 'TBODY', 'TABLE'].includes((mutation.target as HTMLElement).tagName)
-          ) {
+          // TODO: if need add ['COL', 'COLGROUP']
+          if (['TD', 'TR', 'TBODY', 'TABLE'].includes((mutation.target as HTMLElement).tagName)) {
             this.fixTableByLisenter();
             return true;
           }
           return false;
         });
+        for (const mutation of mutations) {
+          const mutationTarget = mutation.target as HTMLElement;
+          if (mutationTarget.tagName === 'TABLE') {
+            const tableMain = Quill.find(mutationTarget) as TableMainFormat;
+            if (tableMain) {
+              tableMain.sortMergeChildren();
+              break;
+            }
+          }
+        }
       },
     );
   }
