aad39a4688ec4d4bd6152d697aa77aa0824e301c
zzxming <zzxming77@163.com>
2025-09-25 19:23:15 +0800

    test: wait event

--- src/__tests__/e2e/utils.ts
+++ src/__tests__/e2e/utils.ts
@@ -2,12 +2,25 @@ import type { Page, PlaywrightWorkerOptions } from '@playwright/test';
 import { test } from '@playwright/test';
 import { EditorPage } from './editor-page';
 
+declare global {
+  interface Window {
+    pasteEventTriggered: boolean;
+  }
+}
+
 export async function createTableBySelect(page: Page, container: string, row: number, col: number) {
   await page.locator(`#${container} .ql-toolbar .ql-table-up.ql-picker`).click();
   await page.locator(`#${container} .ql-toolbar .ql-custom-select .table-up-select-box__item[data-row="${row}"][data-col="${col}"]`).click();
 }
 
 export async function pasteHTML(page: Page, html: string, { browserName = 'chromium' }: { browserName: PlaywrightWorkerOptions['browserName'] }) {
+  await page.evaluate(() => {
+    window.pasteEventTriggered = false;
+    document.addEventListener('paste', () => {
+      window.pasteEventTriggered = true;
+    });
+  });
+
   await page.evaluate(async ({ html, browserName }) => {
     if (browserName === 'firefox') {
       const clipboardItem = new ClipboardItem({ 'text/html': new Blob([html], { type: 'text/html' }) });
@@ -27,7 +40,7 @@ export async function pasteHTML(page: Page, html: string, { browserName = 'chrom
   if (browserName === 'firefox') {
     await page.keyboard.press('Control+v');
   }
-  await page.waitForTimeout(1000);
+  await page.waitForFunction(() => window.pasteEventTriggered);
 }
 
 export const extendTest = test.extend<{
