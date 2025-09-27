9d0e0c80467954e84fd73cea201984234b21b605
zzxming <zzxming77@163.com>
2025-09-27 13:15:03 +0800

    test: ci config

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,11 +5,14 @@ export default defineConfig({
   testDir: './src/__tests__/e2e',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 3 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
     trace: 'on-first-retry',
+    launchOptions: {
+      slowMo: process.env.CI ? 0 : 300,
+    },
   },
   projects: [
     {
