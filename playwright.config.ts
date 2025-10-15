a40f2bcd87773cd8ac683e3309e61ef37f7ebfcc
zzxming <74341337+zzxming@users.noreply.github.com>
2025-10-15 19:15:23 +0800

    chore: reomve worker limit (#254)
    * chore: reomve worker limit
    
    * chore: test worker
    
    * chore: revert

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 3 : 0,
-  workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
     trace: 'on-first-retry',
