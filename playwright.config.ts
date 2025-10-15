e548768f5eddd4961504bd96c7ed59a23d0b967a
zzxming <zzxming77@163.com>
2025-10-15 19:09:47 +0800

    chore: revert

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 3 : 0,
-  workers: '50%',
   reporter: 'html',
   use: {
     trace: 'on-first-retry',
